d688fcc756c4f390d7a707852840921a161b5096
Johan B George <johanb25george@gmail.com>
2026-04-13 10:18:37 +0530

    icon

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -2,9 +2,13 @@ import { SwapTokenContextProvider } from "../Context/SwapContext";
 import { LiquidityProvider } from "../Context/LiquidityContext";
 import {NavBar} from "../Components/index";
 import "../styles/globals.css";
+import Head from "next/head";
 function MyApp({ Component, pageProps }) {
   return (
     <SwapTokenContextProvider>
+      <Head>
+        <title>Project Aura</title>
+      </Head>
       <NavBar/>
         <LiquidityProvider>
           <Component {...pageProps} />
